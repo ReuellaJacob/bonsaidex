30c14f5d2be7d24e9c1d810a6666923e38e5c18a
Steven Phun <phun.steven@gmail.com>
2022-02-18 16:01:17 -0800

    add data, stats, types view

--- models/scripts.js
+++ models/scripts.js
@@ -2,7 +2,8 @@
 let pokemons;
 let abilities;
 let forms;
-// attribute
+
+// attribute names
 const mainView = "pokemonMainView";
 const infoView = "pokemonInfoView";
 const subInfoView = "pokemonSubInfoView";
@@ -34,9 +35,8 @@ function loadDataFromJson() {
         .then(jsondata => {
 
             pokemons = jsondata
-            // TODO: uncomment landing view
-            // drawPokedexGridView()
-            drawPokemonDetailedView(getPokemon("Mewtwo"))
+
+            drawPokedexGridView()
         })
 }
 
@@ -144,6 +144,54 @@ function drawPokemonDetailedView(pokemon) {
 
 function drawDataView(pokemon) {
     clearSubInfoView()
+
+    // tags
+    const subInfoViewTag = document.querySelector(`#${subInfoView}`)
+    const heightLabelTag = document.createElement("p")
+    const weightLabelTag = document.createElement("p")
+    const heightValueTag = document.createElement("p")
+    const weightValueTag = document.createElement("p")
+    const typeLabelTag = document.createElement("p")
+    const dividerTag1 = document.createElement("p")
+    const dividerTag2 = document.createElement("p")
+
+    // content
+    heightLabelTag.innerHTML = "Height"
+    weightLabelTag.innerHTML = "Weight"
+    heightValueTag.innerHTML = pokemon.height
+    weightValueTag.innerHTML = pokemon.weight
+    typeLabelTag.innerHTML = "Type"
+    dividerTag1.innerHTML = " | "
+    dividerTag2.innerHTML = " | "
+
+    // append tags
+    subInfoViewTag.append(heightLabelTag)
+    subInfoViewTag.append(heightValueTag)
+    subInfoViewTag.append(dividerTag1)
+    subInfoViewTag.append(typeLabelTag)
+
+    // pokemon types
+    pokemon.types.forEach((type => {
+        drawTypeIcon(type)
+    }))
+
+    // append tags
+    subInfoViewTag.append(dividerTag2)
+    subInfoViewTag.append(weightLabelTag)
+    subInfoViewTag.append(weightValueTag)
+
+    // pokemon ability
+    pokemon.abilities.forEach((ability) => {
+        drawTitle("Ability")
+        drawDataTextView(ability)
+        drawDataTextView(abilities[ability])
+    })
+
+    // append tag
+    drawTitle("Region")
+    drawDataTextView(getRegion(pokemon))
+    drawTitle("Evolution Chain")
+    drawPokemonEvoChain(pokemon.evolutionNames)
 }
 
 function drawStatsView(pokemon) {
@@ -211,35 +259,99 @@ function drawStatsView(pokemon) {
     }
 
     pokemon.forms.forEach((pokemonName) => {
-        const pokemon = getPokemon(pokemonName)
-        // tags
-        const formTag = document.createElement("p")
-        const image = document.createElement("img")
-        const name = document.createElement("p")
-        const nationdexNum = document.createElement("p")
-
-        // contents
-        formTag.innerHTML = "Forms:"
-        image.src = getPokemonImagePath(pokemon)
-        name.innerHTML = pokemon.name
-        nationdexNum.innerHTML = getPokedexNumber(pokemon)
-
-        // attributes
-        image.setAttribute('class', 'pokemonGridImage')
-
-        // event listner
-        image.addEventListener("click", () => {
-            drawPokemonDetailedView(pokemon)
-        })
-
-        // append tags
-        subInfoViewTag.append(image)
-        subInfoViewTag.append(name)
-        subInfoViewTag.append(nationdexNum)
+        drawPokemonPreview(pokemonName)
     })
 }
-function drawTypesView() {
+
+function drawTypeView(pokemon) {
     clearSubInfoView()
+    const chart = getTypeChart(pokemon.types)
+
+    drawTitle("Super Effective x4")
+    drawTypesGrid(chart[0])
+    drawTitle("Super Effective x2")
+    drawTypesGrid(chart[1])
+    drawTitle("Super Effective x0.5")
+    drawTypesGrid(chart[2])
+    drawTitle("Super Effective x0.25")
+    drawTypesGrid(chart[3])
+    drawTitle("Immunity")
+    drawTypesGrid(chart[4])
+}
+
+/*********** DRAW VIEW HELPERS **********/
+function drawTypesGrid(types) {
+    const view = document.querySelector(`#${subInfoView}`)
+
+    types.forEach((type) => {
+        drawTypeIcon(type)
+    })
+}
+
+function drawPokemonEvoChain(chains) {
+    const view = document.querySelector(`#${subInfoView}`)
+
+    chains.forEach((chain) => {
+        chain.forEach((pokemonName) => {
+            const arrowTag = document.createElement("p")
+            arrowTag.innerHTML = ">"
+            view.append(arrowTag)
+            drawPokemonPreview(pokemonName)
+        })
+    })
+}
+
+function drawPokemonPreview(name) {
+    const pokemon = getPokemon(name)
+
+    // tags
+    const view = document.querySelector(`#${subInfoView}`)
+    const image = document.createElement("img")
+    const nameTag = document.createElement("p")
+    const nationdexNum = document.createElement("p")
+
+    // contents
+    image.src = getPokemonImagePath(pokemon)
+    nameTag.innerHTML = pokemon.name
+    nationdexNum.innerHTML = getPokedexNumber(pokemon)
+
+    // attributes
+    image.setAttribute('class', 'pokemonGridImage')
+
+    // event listner
+    image.addEventListener("click", () => {
+        drawPokemonDetailedView(pokemon)
+    })
+
+    // append tags
+    view.append(image)
+    view.append(nameTag)
+    view.append(nationdexNum)
+}
+
+function drawTitle(title) {
+    const view = document.querySelector(`#${subInfoView}`)
+    const tag = document.createElement("p")
+    tag.innerHTML = `${title}:`
+    tag.classList.add("dataTitle")
+    view.append(tag)
+}
+
+function drawDataTextView(text) {
+    const view = document.querySelector(`#${subInfoView}`)
+    const tag = document.createElement("p")
+    tag.innerHTML = text
+    tag.classList.add("dataText")
+    view.append(tag)
+}
+
+function drawTypeIcon(type) {
+    const view = document.querySelector(`#${subInfoView}`)
+    const tag = document.createElement("img")
+    tag.src = `images/types/${type.toLowerCase()}.svg`
+    tag.classList.add(type.toLowerCase())
+    tag.classList.add("typeIcon")
+    view.append(tag)
 }
 
 /*********** CLEAR VIEW FUNCTIONS **********/
@@ -283,4 +395,220 @@ function getPokemonImagePath(pokemon) {
     }
 
     return `images/pokemons/${pokemon.name}.imageset/${pokemon.id}.png`
+}
+
+function getRegion(pokemon) {
+    const pokemonID = pokemon.id
+
+    if (pokemonID <= 151) {
+        return "Kanto"
+    } else if (pokemonID <= 251) {
+        return "Johto"
+    } else if (pokemonID <= 386) {
+        return "Hoenn"
+    } else if (pokemonID <= 493) {
+        return "Sinnoh"
+    } else if (pokemonID <= 649) {
+        return "Unova"
+    } else if (pokemonID <= 721) {
+        return "Kalos"
+    } else if (pokemonID <= 809) {
+        return "Alola"
+    } else if (pokemonID <= 905) {
+        return "Galar"
+    } else {
+        return "No region found."
+    }
+}
+
+function getTypeChart(types) {
+    let array = [[], [], [], [], []]
+    let chart = {"Bug": 1, "Dark": 1, "Dragon": 1,"Electric": 1, "Fairy": 1, "Fighting": 1 ,"Fire": 1, "Flying": 1, "Ghost": 1,"Grass": 1, "Ground": 1, "Ice": 1,"Normal": 1, "Poison": 1, "Psychic": 1,"Rock": 1, "Steel": 1, "Water": 1}
+
+    // get type values for each type
+    types.forEach((type) => {
+        if (type === "Normal") {
+            chart["Fighting"] *= 2
+
+            chart["Ghost"] *= 0
+        } else if (type === "Fire") {
+            chart["Water"] *= 2
+            chart["Ground"] *= 2
+            chart["Rock"] *= 2
+
+            chart["Fire"] *= 0.5
+            chart["Grass"] *= 0.5
+            chart["Ice"] *= 0.5
+            chart["Bug"] *= 0.5
+            chart["Steel"] *= 0.5
+            chart["Fairy"] *= 0.5
+        } else if (type === "Water") {
+            chart["Electric"] *= 2
+            chart["Grass"] *= 2
+
+            chart["Fire"] *= 0.5
+            chart["Water"] *= 0.5
+            chart["Ice"] *= 0.5
+            chart["Steel"] *= 0.5
+        } else if (type === "Electric") {
+            chart["Ground"] *= 2
+
+            chart["Electric"] *= 0.5
+            chart["Flying"] *= 0.5
+            chart["Steel"] *= 0.5
+        } else if (type === "Grass") {
+            chart["Fire"] *= 2
+            chart["Ice"] *= 2
+            chart["Poison"] *= 2
+            chart["Flying"] *= 2
+            chart["Bug"] *= 2
+
+            chart["Water"] *= 0.5
+            chart["Electric"] *= 0.5
+            chart["Grass"] *= 0.5
+            chart["Ground"] *= 0.5
+        } else if (type === "Ice") {
+            chart["Fire"] *= 2
+            chart["Fighting"] *= 2
+            chart["Rock"] *= 2
+            chart["Steel"] *= 2
+
+            chart["Ice"] *= 0.5
+        } else if (type === "Fighting") {
+            chart["Flying"] *= 2
+            chart["Psychic"] *= 2
+            chart["Fairy"] *= 2
+
+            chart["Bug"] *= 0.5
+            chart["Rock"] *= 0.5
+            chart["Dark"] *= 0.5
+        } else if (type === "Poison") {
+            chart["Ground"] *= 2
+            chart["Psychic"] *= 2
+
+            chart["Grass"] *= 0.5
+            chart["Fighting"] *= 0.5
+            chart["Poison"] *= 0.5
+            chart["Bug"] *= 0.5
+            chart["Fairy"] *= 0.5
+        } else if (type === "Ground") {
+            chart["Water"] *= 2
+            chart["Grass"] *= 2
+            chart["Ice"] *= 2
+
+            chart["Poison"] *= 0.5
+            chart["Rock"] *= 0.5
+
+
+            chart["Electric"] *= 0
+        } else if (type === "Flying") {
+            chart["Electric"] *= 2
+            chart["Ice"] *= 2
+            chart["Rock"] *= 2
+
+            chart["Grass"] *= 0.5
+            chart["Fighting"] *= 0.5
+            chart["Bug"] *= 0.5
+
+            chart["Ground"] *= 0
+        } else if (type === "Psychic") {
+            chart["Bug"] *= 2
+            chart["Ghost"] *= 2
+            chart["Dark"] *= 2
+
+            chart["Fighting"] *= 0.5
+            chart["Psychic"] *= 0.5
+        } else if (type === "Bug") {
+            chart["Fire"] *= 2
+            chart["Flying"] *= 2
+            chart["Rock"] *= 2
+
+            chart["Grass"] *= 0.5
+            chart["Fighting"] *= 0.5
+            chart["Ground"] *= 0.5
+        } else if (type === "Rock") {
+            chart["Water"] *= 2
+            chart["Grass"] *= 2
+            chart["Fighting"] *= 2
+            chart["Ground"] *= 2
+            chart["Steel"] *= 2
+
+            chart["Normal"] *= 0.5
+            chart["Fire"] *= 0.5
+            chart["Poison"] *= 0.5
+            chart["Flying"] *= 0.5
+        } else if (type === "Ghost") {
+            chart["Ghost"] *= 2
+            chart["Dark"] *= 2
+
+            chart["Poison"] *= 0.5
+            chart["Bug"] *= 0.5
+
+            chart["Normal"] *= 0
+            chart["Fighting"] *= 0
+        } else if (type === "Dragon") {
+            chart["Ice"] *= 2
+            chart["Dragon"] *= 2
+            chart["Fairy"] *= 2
+
+            chart["Fire"] *= 0.5
+            chart["Water"] *= 0.5
+            chart["Electric"] *= 0.5
+            chart["Grass"] *= 0.5
+        } else if (type === "Dark") {
+            chart["Fighting"] *= 2
+            chart["Bug"] *= 2
+            chart["Fairy"] *= 2
+
+            chart["Ghost"] *= 0.5
+            chart["Dark"] *= 0.5
+
+            chart["Psychic"] *= 0
+        } else if (type === "Steel") {
+            chart["Fire"] *= 2
+            chart["Fighting"] *= 2
+            chart["Ground"] *= 2
+
+            chart["Normal"] *= 0.5
+            chart["Grass"] *= 0.5
+            chart["Ice"] *= 0.5
+            chart["Flying"] *= 0.5
+            chart["Psychic"] *= 0.5
+            chart["Bug"] *= 0.5
+            chart["Rock"] *= 0.5
+            chart["Dragon"] *= 0.5
+            chart["Steel"] *= 0.5
+            chart["Fairy"] *= 0.5
+
+            chart["Poison"] *= 0
+        } else if (type === "Fairy") {
+            chart["Poison"] *= 2
+            chart["Steel"] *= 2
+
+            chart["Fighting"] *= 0.5
+            chart["Bug"] *= 0.5
+            chart["Dark"] *= 0.5
+
+            chart["Dragon"] *= 0
+        }
+    })
+
+    // get type value
+    const keys = Object.keys(chart)
+    keys.forEach((key) => {
+        const value = chart[key]
+        if (value === 4) {
+            array[0].push(key)
+        } else if (value === 2) {
+            array[1].push(key)
+        } else if (value === 0.5) {
+            array[2].push(key)
+        } else if (value === 0.25) {
+            array[3].push(key)
+        } else if (value === 0) {
+            array[4].push(key)
+        }
+    })
+
+    return array
 }
